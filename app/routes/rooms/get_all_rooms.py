c4b68745f222b1333a91b53917a629758472cb7f
takuya0509 <matsumoto@intersect.inc>
2024-05-12 04:11:33 +0900

    include closed_at

--- app/routes/rooms/get_all_rooms.py
+++ app/routes/rooms/get_all_rooms.py
@@ -1,11 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from crud.room import get_all_rooms
 from schema import APIBaseModel
+from datetime import datetime
 
 
 class Room(APIBaseModel):
     uuid: str
     name: str
+    closed_at: datetime | None
 
 
 class GetAllRoomsResponse(APIBaseModel):
@@ -15,5 +17,8 @@ class GetAllRoomsResponse(APIBaseModel):
 async def handler(session: AsyncSession) -> GetAllRoomsResponse:
     rooms = await get_all_rooms(session)
     return GetAllRoomsResponse(
-        rooms=[Room(uuid=room.uuid, name=room.name) for room in rooms]
+        rooms=[
+            Room(uuid=room.uuid, name=room.name, closed_at=room.closed_at)
+            for room in rooms
+        ]
     )
